1861d665996a4f17abf549378904ee1532f1ca08
feller2021 <100558543+feller2021@users.noreply.github.com>
2024-03-08 13:23:18 +0800

    更新 diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,7 +22,7 @@ sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz
 # Add a feed source
 echo '我证明diy-part1.sh脚本已经成功运行'
 # 科学插件
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >>feeds.conf.default
 echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;main' >>feeds.conf.default
 #echo 'src-git bypass https://github.com/tianiue/luci-app-bypass.git' >>feeds.conf.default
